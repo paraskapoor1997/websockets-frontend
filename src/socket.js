2ee54da2d2dc6439bb0f03d06a48555a9864f664
paras kapoor <kapoorparas1997@gmail.com>
2025-10-27 00:00:31 +0530

    updating socket config for prod

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,9 @@
 import io from 'socket.io-client';
 
 export const createSocketConnection = () => {
-    return io(`${process.env.REACT_APP_API_URL}`); // Backend server URL
+    if(location.hostname ==='localhost'){
+        return io(`${process.env.REACT_APP_API_URL}`); // Backend server URL
+    }else{
+        return io('/', {path: '/api/socket.io'});
+    }
 };
