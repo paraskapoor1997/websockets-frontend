cc5faf64e3cfc28fb4c8520128c80d6830d2c10e
paras kapoor <kapoorparas1997@gmail.com>
2025-10-27 00:04:05 +0530

    fix socket linting error

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,7 @@
 import io from 'socket.io-client';
 
 export const createSocketConnection = () => {
-    if(location.hostname ==='localhost'){
+    if(window.location.hostname ==='localhost'){
         return io(`${process.env.REACT_APP_API_URL}`); // Backend server URL
     }else{
         return io('/', {path: '/api/socket.io'});
